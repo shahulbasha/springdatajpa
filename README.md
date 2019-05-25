759e0be565c2a83acce9a7561d210bbbdb7bc0c9
ShahulBasha <shhl.basha333@gmail.com>
2019-05-25 23:47:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple Spring Data JPA  project and how it simplifies traditional spring projects
---
+++ src/main/java/org/shahul/springboot/bootRESTCRUD/BootRestcrudApplication.java
@@ -0,0 +1,13 @@
+package org.shahul.springboot.bootRESTCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestcrudApplication.class, args);
+	}
+
+}
